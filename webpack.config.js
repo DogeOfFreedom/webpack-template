fd39c23ac7b24c0f9febd52e4af68b2cf290ec37
DogeOfFreedom <jchensi2000@gmail.com>
2024-02-10 17:23:37 +1100

    initial template repo commit

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+    entry: './src/scripts/index.js',
+    output: {
+      filename: 'bundle.js',
+      path: path.resolve(__dirname, 'dist'),
+    },
+    module: {
+      rules: [{
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.(jpg|jpeg|png|otf|ttf)$/i,
+        type: "asset/resource"
+      }]  
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: "./src/index.html",
+        filename: "index.html",
+        inject: "head",
+        scriptLoading: "defer"
+      })
+    ]
+  };
